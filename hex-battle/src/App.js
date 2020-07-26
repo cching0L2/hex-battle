00d24a41a9cc16f95164f6976dddecf6dfbdb3de
Camellia Peng <camelliapeng02@gmail.com>
2020-07-26 16:15:27 -0700

    Set up react project

---
+++ hex-battle/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HEX BATTLE</h1>
+      <h2>#FFFFFF</h2>
+      <h2>embed a color picker here</h2>
+    </div>
+  );
+}
+
+export default App;
