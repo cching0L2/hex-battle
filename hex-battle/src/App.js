0133b55198631d11836fb12f7fc0549b42ec53c3
Camellia Peng <camelliapeng02@gmail.com>
2020-07-27 09:03:51 -0700

    Add styling

--- hex-battle/src/App.js
+++ hex-battle/src/App.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import './App.css';
+import reactCSS from 'reactcss'
 import ColorPicker from './ColorPicker';
 import { ColorWrap } from 'react-color/lib/components/common';
+
+const styles = reactCSS({
+  'default': {
+    App: {
+      textAlign: 'center'
+    },
+    title: {
+      color: '#55868C',
+      fontSize: '40px'
+    },
+    titleContainer: {
+      marginTop: '48px'
+    },
+    statsContainer: {
+      marginTop: '36px',
+    },
+    stats: {
+      margin: '8px 0px',
+      color: "#474056"
+    },
+    instContainer: {
+      marginTop: '36px',
+    },
+    instruction: {
+      color: '#D90368'
+    },
+    pickerContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '24px 0px'
+    },
+    yourPick: {
+      color: "#474056",
+      marginTop: '32px'
+    },
+    chosen: {
+      height: "60px",
+      width: "100px",
+      border: "1px solid #AAA",
+      margin: "auto"
+    },
+    confirm: {
+      backgroundColor: 'transparent',
+      border: 'none',
+      fontFamily: "Nunito",
+      marginTop: '24px',
+      fontSize: '22px',
+      fontWeight: 'bold'
+    }
+  },
+})
 
 class App extends React.Component {
   state = {
@@ -17,28 +70,36 @@ class App extends React.Component {
     this.setState({currentColor: e.hex});
   }
 
+  onConfirm = (e) => {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="title-container">
-          <h1 className="title">HEX BATTLE</h1>
+      <div style={ styles.App }>
+        <div style={ styles.titleContainer }>
+          <h1 style={ styles.title }>HEX BATTLE</h1>
         </div>
-        <div className="stats-container">
-          <h3>ROUND {this.state.currentRound}/{this.state.totalRound}</h3>
-          <h3>SCORE: {this.state.totalScore}</h3>
+        <div style={ styles.statsContainer }>
+          <h3 style={ styles.stats }>ROUND {this.state.currentRound}/{this.state.totalRound}</h3>
+          <h3 style={ styles.stats }>SCORE: {this.state.totalScore}</h3>
         </div>
-        <div className="inst-container">
+        <div style={ styles.statsContainer }>
+          <h4 style={ styles.instruction }>Use color picker below to pick out the color:</h4>
           <h2>{this.state.goalColor.toUpperCase()}</h2>
         </div>
-        <div className="picker-container">
+        <div style={ styles.pickerContainer }>
           <ColorPicker 
             onChangeComplete={this.onChange}
             color={this.state.currentColor}
           />
         </div>
-        <h3>Your pick:</h3>
-        <div className="chosen" style={{backgroundColor: this.state.currentColor}}></div>
-        <h2>Confirm</h2>
+        <h3 style={ styles.yourPick }>Your pick:</h3>
+        <div style={ Object.assign({backgroundColor: this.state.currentColor}, styles.chosen) }></div>
+        <button
+          style={ styles.confirm }
+          onClick={ this.onConfirm }
+        >Confirm</button>
       </div>
     );
   }
